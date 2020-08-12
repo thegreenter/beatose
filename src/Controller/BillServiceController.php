b637e59b6359f4c33706e20aa6829f1fb8521ccd
Giancarlos Salas <giansalex@gmail.com>
2020-08-12 17:54:34 -0500

    Add Soap service endpoint

--- src/Controller/BillServiceController.php
+++ src/Controller/BillServiceController.php
@@ -4,12 +4,51 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Services\BillServiceInterface;
+use SoapServer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class BillServiceController extends AbstractController
 {
-    public function index()
+    /**
+     * @var BillServiceInterface
+     */
+    private $billService;
+
+    /**
+     * BillServiceController constructor.
+     *
+     * @param BillServiceInterface $billService
+     */
+    public function __construct(BillServiceInterface $billService)
     {
-        return $this->json(['message' => 'Soap Server']);
+        $this->billService = $billService;
+    }
+
+    public function index(Request $request)
+    {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/xml; charset=utf-8');
+        $url = $this->generateUrl('bill_service', [], UrlGeneratorInterface::ABSOLUTE_URL);
+        $wsdlPath = __DIR__.'/../../public/billService.wsdl';
+
+        if ($request->query->has('wsdl')) {
+            $wsdl = file_get_contents($wsdlPath);
+
+            return $response->setContent(str_replace('%URL_SERVICE%', $url, $wsdl));
+        }
+
+        $options = ['uri' => $url];
+        $soapServer = new SoapServer($wsdlPath, $options);
+        $soapServer->setObject($this->billService);
+
+        ob_start();
+        $soapServer->handle();
+        $response->setContent(ob_get_clean());
+
+        return $response;
     }
 }
