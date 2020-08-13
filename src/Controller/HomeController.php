30d6f59b3ad52b3919b558cec6820619be1dbfd0
Giancarlos Salas <giansalex@gmail.com>
2020-08-13 14:53:14 -0500

    Redirect index to billService

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->redirectToRoute('bill_service', ['wsdl' => '']);
+    }
+}
