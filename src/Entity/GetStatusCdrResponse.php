53b8145060e96a6a05c267fd2f2d4ec545d67fd7
Giancarlos Salas <giansalex@gmail.com>
2020-08-13 14:32:41 -0500

    Add getStatusCdr Response

---
+++ src/Entity/GetStatusCdrResponse.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class GetStatusCdrResponse
+{
+    /**
+     * @var StatusCdrResponse
+     */
+    private $statusCdr;
+}
---
+++ src/Entity/StatusCdrResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class StatusCdrResponse
+{
+    /**
+     * @var string
+     */
+    private $content;
+    /**
+     * @var string
+     */
+    private $statusCode;
+    /**
+     * @var string
+     */
+    private $statusMessage;
+}
