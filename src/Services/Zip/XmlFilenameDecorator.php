e9bb56e4569d073aa23eec825ad2e37c8e071388
Giancarlos Salas <giansalex@gmail.com>
2020-08-19 23:45:29 -0500

    Add xml zip implementation

---
+++ src/Services/Zip/XmlFilenameDecorator.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Zip;
+
+use App\Entity\ValidationError;
+use App\Entity\ValueResult;
+
+class XmlFilenameDecorator implements XmlZipInterface
+{
+    /**
+     * @var XmlZipInterface
+     */
+    private $xmlZip;
+
+    /**
+     * XmlFilenameDecorator constructor.
+     * @param XmlZipInterface $xmlZip
+     */
+    public function __construct(XmlZipInterface $xmlZip)
+    {
+        $this->xmlZip = $xmlZip;
+    }
+
+    /**
+     * @param string $filename
+     * @param string|null $content
+     * @return ValueResult
+     */
+    public function compress(string $filename, ?string $content): ValueResult
+    {
+        $info = pathinfo($filename);
+
+        if (strtoupper($info['extension']) !== 'ZIP') {
+            return (new ValueResult())->setError(new ValidationError('0151'));
+        }
+
+        $xmlFilename = $info['filename'].'.xml';
+        return $this->xmlZip->compress($xmlFilename, $content);
+    }
+
+    /**
+     * @param string|null $zip
+     * @param string $filename
+     * @return ValueResult
+     */
+    public function decompress(?string $zip, string $filename): ValueResult
+    {
+        $info = pathinfo($filename);
+
+        if (strtoupper($info['extension']) !== 'ZIP') {
+            return (new ValueResult())->setError(new ValidationError('0151'));
+        }
+
+        $xmlFilename = $info['filename'].'.xml';
+        return $this->xmlZip->decompress($zip, $xmlFilename);
+    }
+}
---
+++ src/Services/Zip/XmlZipFly.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace App\Services\Zip;
+
+use App\Entity\ErrorCodeList;
+use App\Entity\ValidationError;
+use App\Entity\ValueResult;
+use PhpZip\Exception\ZipException;
+use PhpZip\ZipFile;
+
+class XmlZipFly implements XmlZipInterface
+{
+    public function compress(string $filename, ?string $content): ValueResult
+    {
+        $zipFile = new ZipFile();
+        $zipFile->addFromString($filename, $content);
+        $zip = $zipFile->outputAsString();
+        $zipFile->close();
+
+        return (new ValueResult())->setContent($zip);
+    }
+
+    public function decompress(?string $zip, string $filename): ValueResult
+    {
+        $zipFile = $this->openZip($zip);
+        $result = new ValueResult();
+        if ($zipFile === null) {
+            return $result->setError(new ValidationError(ErrorCodeList::ZIP_CORRUPTO));
+        }
+
+        if ($zipFile->count() === 0) {
+            return $result->setError(new ValidationError(ErrorCodeList::ZIP_EMPTY));
+        }
+
+        if (!$zipFile->hasEntry($filename)) {
+            return $result->setError(new ValidationError(ErrorCodeList::ZIP_SIN_CPE));
+        }
+
+        try {
+            $xml = $zipFile->getEntryContents($filename);
+            return $result->setContent($xml);
+        } catch (ZipException $e) {
+            return $result->setError(new ValidationError(ErrorCodeList::ZIP_ERROR_UNZIP));
+        } finally {
+            $zipFile->close();
+        }
+    }
+
+    private function openZip($zip): ?ZipFile
+    {
+        try {
+            $zipFile = new ZipFile();
+            return $zipFile->openFromString($zip);
+        } catch (ZipException $e) {
+            return null;
+        }
+    }
+}
