484d3d7266313ef02f9ef9d4e625ba4dc567b65e
Giancarlos Salas <giansalex@gmail.com>
2020-08-19 22:52:40 -0500

    Add signature validator

---
+++ src/Validator/XmlSignValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Validator;
+
+use App\Entity\ValidationError;
+use DOMDocument;
+use Greenter\XMLSecLibs\Sunat\SignedXml;
+
+class XmlSignValidator implements XmlValidatorInterface
+{
+    /**
+     * @var SignedXml
+     */
+    private $signer;
+
+    /**
+     * XmlSignValidator constructor.
+     * @param SignedXml $signer
+     */
+    public function __construct(SignedXml $signer)
+    {
+        $this->signer = $signer;
+    }
+
+    public function validate(string $filename, DOMDocument $document): ?ValidationError
+    {
+        return $this->signer->verify($document) ? null : new ValidationError('2334');
+    }
+}
