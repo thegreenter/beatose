69b10edf8122025b056bf2e67464aa005b974f8f
Giancarlos Salas <giansalex@gmail.com>
2020-09-14 19:11:05 -0500

    Register ubl services

---
+++ src/Validator/MultiValidator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Validator;
+
+use App\Entity\ValidationError;
+use DOMDocument;
+
+class MultiValidator implements XmlValidatorInterface
+{
+    /**
+     * @var XmlValidatorInterface[]
+     */
+    private $validators;
+
+    /**
+     * MultiValidator constructor.
+     *
+     * @param XmlValidatorInterface[] $validators
+     */
+    public function __construct(array $validators)
+    {
+        $this->validators = $validators;
+    }
+
+    /**
+     * @param string $filename
+     * @param DOMDocument $document
+     * @return ValidationError|null
+     */
+    public function validate(string $filename, DOMDocument $document): ?ValidationError
+    {
+        foreach ($this->validators as $validator) {
+            $error = $validator->validate($filename, $document);
+            if ($error !== null) {
+                return $error;
+            }
+        }
+
+        return null;
+    }
+}
