51bbdcd39c4af23bd9c9651838447744e3788f8a
Giancarlos Salas <giansalex@gmail.com>
2020-09-15 11:04:29 -0500

    Move mapper to util

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,4 +2,4 @@
 
 namespace PHPSTORM_META;
 
-override(\App\Services\Mapper::map(0, 1), type(1));
+override(\App\Services\Util\Mapper::map(0, 1), type(1));
--- src/Services/Util/Mapper.php
+++ src/Services/Util/Mapper.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Services;
+namespace App\Services\Util;
 
 class Mapper
 {
