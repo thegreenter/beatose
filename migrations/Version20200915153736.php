afbafede5d00ddc0657b2e2123ea847b9ef0b87f
Giancarlos Salas <giansalex@gmail.com>
2020-09-15 10:44:26 -0500

    Add initial migration

---
+++ migrations/Version20200915153736.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200915153736 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE cpe_document (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(64) NOT NULL, issuer VARCHAR(11) NOT NULL, state_code VARCHAR(16) DEFAULT NULL, hash_cpe VARCHAR(255) DEFAULT NULL, hash_cdr VARCHAR(255) DEFAULT NULL, ticket VARCHAR(64) DEFAULT NULL)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE cpe_document');
+    }
+}
