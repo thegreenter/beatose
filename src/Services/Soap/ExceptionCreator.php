fc4b3f4c17864f1a095b4f58c8d22beefe736a4a
Giancarlos Salas <giansalex@gmail.com>
2020-09-14 21:53:33 -0500

    Register error code provider

--- src/Services/Soap/ExceptionCreator.php
+++ src/Services/Soap/ExceptionCreator.php
@@ -5,15 +5,31 @@ declare(strict_types=1);
 namespace App\Services\Soap;
 
 use App\Entity\ValidationError;
+use Greenter\Validator\ErrorCodeProviderInterface;
 use SoapFault;
 
 class ExceptionCreator
 {
+    /**
+     * @var ErrorCodeProviderInterface
+     */
+    private $codeResolver;
+
+    /**
+     * ExceptionCreator constructor.
+     *
+     * @param ErrorCodeProviderInterface $codeResolver
+     */
+    public function __construct(ErrorCodeProviderInterface $codeResolver)
+    {
+        $this->codeResolver = $codeResolver;
+    }
+
     public function fromValidation(ValidationError $error): SoapFault
     {
         return new SoapFault(
             $error->getCode(),
-            '--from-xcodes-provider-',
+            $this->codeResolver->getValue($error->getCode()),
             null,
             $error->getDetail(),
         );
