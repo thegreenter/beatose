536de670720ea0efda406e5beddae45eb721068d
Giancarlos Salas <giansalex@gmail.com>
2020-09-15 12:38:47 -0500

    Use greenter cpe-validator

--- src/Validator/CpeXslValidator.php
+++ src/Validator/CpeXslValidator.php
@@ -5,12 +5,43 @@ declare(strict_types=1);
 namespace App\Validator;
 
 use App\Model\ValidationError;
+use App\Services\Xml\XslDocResolverInterface;
 use DOMDocument;
+use Greenter\Validator\Xml\XslValidator;
 
 class CpeXslValidator implements XmlValidatorInterface
 {
+    private XslValidator $validator;
+
+    private XslDocResolverInterface $xslDocResolver;
+
+    /**
+     * CpeXslValidator constructor.
+     *
+     * @param XslValidator $validator
+     * @param XslDocResolverInterface $xslDocResolver
+     */
+    public function __construct(XslValidator $validator, XslDocResolverInterface $xslDocResolver)
+    {
+        $this->validator = $validator;
+        $this->xslDocResolver = $xslDocResolver;
+    }
+
     public function validate(string $filename, DOMDocument $document): ?ValidationError
     {
-        return null;
+        $xslDoc = $this->xslDocResolver->fromDoc($document);
+        if ($xslDoc === null) {
+            return null;
+        }
+
+        $this->validator->setXsl($xslDoc);
+        $result = $this->validator->validate($filename, $document);
+        if (count($result) === 0) {
+            return null;
+        }
+
+        $mapErrors = array_map(fn($error) => new ValidationError($error->getCode(), $error->getMessage()), $result);
+
+        return $mapErrors[0];
     }
 }
