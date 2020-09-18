65cec7a6f846ec0d5172217dfd5f1059c2844ab9
Giancarlos Salas <giansalex@gmail.com>
2020-09-17 21:32:15 -0500

    Add system file store

---
+++ src/Services/File/SystemFileStore.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\File;
+
+use Psr\Log\LoggerInterface;
+
+class SystemFileStore implements FileStoreInterface
+{
+    /**
+     * Directory to save files.
+     *
+     * @var string
+     */
+    private string $uploadDir;
+
+    private LoggerInterface $logger;
+
+    /**
+     * SystemFileStore constructor.
+     * @param string $uploadDir
+     * @param LoggerInterface $logger
+     */
+    public function __construct(string $uploadDir, LoggerInterface $logger)
+    {
+        $this->uploadDir = $uploadDir;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function save(string $filename, ?string $content): void
+    {
+        $fullPath = $this->uploadDir.DIRECTORY_SEPARATOR.$filename;
+        file_put_contents($fullPath, $content);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get(string $filename): ?string
+    {
+        $fullPath = $this->uploadDir.DIRECTORY_SEPARATOR.$filename;
+        if (!file_exists($fullPath)) {
+            $this->logger->warning('File not found: '.$fullPath);
+            return null;
+        }
+
+        $content = file_get_contents($fullPath);
+        if ($content === false) {
+            $this->logger->warning('File cannot read: '.$fullPath);
+            return null;
+        }
+
+        return $content;
+    }
+}
