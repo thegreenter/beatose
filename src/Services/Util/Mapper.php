b587c058246a80225aefdb7f392bd749fe4319bd
Giancarlos Salas <giansalex@gmail.com>
2020-11-29 14:17:33 -0500

    Fix get properties from class

--- src/Services/Util/Mapper.php
+++ src/Services/Util/Mapper.php
@@ -9,7 +9,7 @@ class Mapper
     public function map(object $data, string $classType): object
     {
         $target = new $classType();
-        $props = get_object_vars($target);
+        $props = get_class_vars($classType);
 
         foreach ($props as $key => $value) {
             if (property_exists($data, $key)) {
