0c8c4f5bed76d7ec4079e16edb437b871a0d59a5
Giancarlos Salas <giansalex@gmail.com>
2021-03-06 14:20:16 -0500

    PHP 8 doesn't recognize $detail parameter name

--- src/Services/Soap/ExceptionCreator.php
+++ src/Services/Soap/ExceptionCreator.php
@@ -31,7 +31,8 @@ class ExceptionCreator
         return new SoapFault(
             $error->getCode(),
             $this->codeResolver->getValue($error->getCode()),
-            detail: $detail,
+            null,
+            $detail,
         );
     }
 
