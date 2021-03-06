52b7ee968936ee26f050945fec16d3567c7afa82
Giancarlos Salas <giansalex@gmail.com>
2021-03-05 22:44:27 -0500

    Remove fileinfo ext

--- src/Services/Zip/XmlZipFly.php
+++ src/Services/Zip/XmlZipFly.php
@@ -7,6 +7,7 @@ namespace App\Services\Zip;
 use App\Model\ErrorCodeList;
 use App\Model\ValidationError;
 use App\Model\ValueResult;
+use PhpZip\Constants\ZipCompressionMethod;
 use PhpZip\Exception\ZipException;
 use PhpZip\ZipFile;
 
@@ -15,7 +16,7 @@ class XmlZipFly implements XmlZipInterface
     public function compress(string $filename, ?string $content): ValueResult
     {
         $zipFile = new ZipFile();
-        $zipFile->addFromString($filename, $content);
+        $zipFile->addFromString($filename, $content, ZipCompressionMethod::DEFLATED);
         $zip = $zipFile->outputAsString();
         $zipFile->close();
 
@@ -41,7 +42,7 @@ class XmlZipFly implements XmlZipInterface
         try {
             $xml = $zipFile->getEntryContents($filename);
             return $result->setContent($xml);
-        } catch (ZipException $e) {
+        } catch (ZipException) {
             return $result->setError(new ValidationError(ErrorCodeList::ZIP_ERROR_UNZIP));
         } finally {
             $zipFile->close();
@@ -53,7 +54,7 @@ class XmlZipFly implements XmlZipInterface
         try {
             $zipFile = new ZipFile();
             return $zipFile->openFromString($zip);
-        } catch (ZipException $e) {
+        } catch (ZipException) {
             return null;
         }
     }
